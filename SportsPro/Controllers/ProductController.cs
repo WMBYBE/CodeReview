adc78df3405d171ff36f1cdd63576180c03bbf44
WMBYBE <williambbrouillette@gmail.com>
2025-01-14 11:50:29 -0600

    Product controller and naming

---
+++ SportsPro/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+using SportsPro.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SportsPro.Controllers
+{
+    public class ProductController : Controller
+    {
+        private SportsProContext context { get; set; }
+
+        public ProductController(SportsProContext ctx)
+        {
+            context = ctx;
+        }
+        public IActionResult List()
+        {
+            var products = context.Products.OrderBy(c => c.Name).ToList(); //Sends the lsit of forums to the index page so that you can see them all
+
+            return View(products);
+        }
+    }
+}
