b154b02aca5c71d614abee7969002b761b14508a
WMBYBE <williambbrouillette@gmail.com>
2025-01-23 10:17:46 -0600

    update

--- SportsPro/Controllers/ProductController.cs
+++ SportsPro/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
 using SportsPro.Models;
 using System.Linq;
 
@@ -14,13 +15,25 @@ namespace SportsPro.Controllers
         }
         public IActionResult List()
         {
-            var products = context.Products.OrderBy(c => c.ReleaseDate).ToList(); //Sends the lsit of forums to the index page so that you can see them all
-
+            var products = context.Products.OrderBy(c => c.ReleaseDate).ToList();
             return View(products);
         }
         public IActionResult addProduct()
         {
+            // create new Product object
+            Product product = new Product();                
+
             ViewBag.Action = "Add";
+
+            // bind product to AddUpdate view
+            return View("addProduct", product);
+        }
+
+        public IActionResult edit(int id)
+        {
+            Product product = context.Products.FirstOrDefault(p => p.ProductID == id);
+            ViewBag.Action = "Edit";
+
             return View("addProduct", new Product());
         }
         [HttpPost]
@@ -28,7 +41,7 @@ namespace SportsPro.Controllers
         {
             if (ModelState.IsValid)
             {
-                if (product.ProductCode == " ")
+                if (product.ProductID == 0)
                     context.Products.Add(product);
                 else
                     context.Products.Update(product);
@@ -37,7 +50,7 @@ namespace SportsPro.Controllers
             }
             else
             {
-                ViewBag.Action = (product.ProductCode == " ") ? "Add" : "Edit";
+                ViewBag.Action = (product.ProductID == 0) ? "Add" : "Edit";
                 return View(product);
             }
         }
