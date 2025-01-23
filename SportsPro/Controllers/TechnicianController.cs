2805400813701cd557cf76b7b66126d93df6c67f
Grym3038 <156126041+Grym3038@users.noreply.github.com>
2025-01-22 20:30:30 -0600

    forgot to push changes

---
+++ SportsPro/Controllers/TechnicianController.cs
@@ -0,0 +1,203 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SportsPro.Models;
+using System.Linq;
+
+namespace SportsPro.Controllers
+{
+    public class TechnicianController : Controller
+    {
+        private SportsProContext _context;
+        public TechnicianController(SportsProContext context)
+        {
+            _context = context;
+        }
+        public IActionResult List()
+        {
+            var techs = _context.Technicians.ToList();
+            return View(techs);
+        }
+
+
+        [NonAction]
+        public void ClearChangeTracker()
+        {
+            foreach (var entry in _context.ChangeTracker.Entries().ToList())
+            {
+                entry.State = EntityState.Detached;
+            }
+        }
+
+
+        [NonAction]
+        private void ValidateTeamEditViewModel(TechEditViewModel model)
+        {
+            // Ensure the team name is unique
+            var tech = _context.Technicians
+                .Where(t => t.TechnicianID != model.Technician!.TechnicianID && t.Name == model.Technician.Name)
+                .FirstOrDefault();
+
+
+            if (tech != null)
+            {
+                ModelState.AddModelError("Character.Name", "You already have a character with that name.");
+            }
+
+        }
+
+
+        [HttpGet]
+        [Route("/tech/add/")]
+        public ActionResult Add()
+        {
+            var model = new TechEditViewModel()
+            {
+                Mode = "Add",
+                Technician = new Technician()
+            };
+
+
+            return View("Edit", model);
+        }
+
+        [HttpPost]
+        [Route("/tech/add/")]
+        public ActionResult Add(TechEditViewModel model)
+        {
+            model.Mode = "Add";
+
+            // Ensure the user is logged in
+
+
+            ValidateTeamEditViewModel(model);
+
+            // Show the add form again if there were validation errors
+            if (!ModelState.IsValid)
+            {
+                return View("Edit", model);
+            }
+
+            // Add the team
+
+            _context.Add(model.Technician);
+            _context.SaveChanges();
+
+            // Redirect to the user's teams page
+            TempData["message"] = $"You just added the team {model.Technician.Name}.";
+            return RedirectToAction("List", "Technician");
+        }
+
+        [HttpGet]
+        [Route("/tech/{id}/edit/")]
+        public ActionResult Edit(int id)
+        {
+            var model = new TechEditViewModel()
+            {
+                Mode = "Edit",
+                Technician = new Technician()
+            };
+
+
+
+            // Ensure the team exists and is owned by the user
+            model.Technician = _context.Technicians
+                .Where(t => t.TechnicianID == id)
+                .FirstOrDefault();
+
+            if (model.Technician == null)
+            {
+                return NotFound();
+            }
+
+            return View("Edit", model);
+        }
+
+        [HttpPost]
+        [Route("/tech/{id}/edit/")]
+        public ActionResult Edit(int id, TechEditViewModel model)
+        {
+
+
+            // Ensure the team exists and is owned by the user
+            var tech = _context.Technicians
+                .Where(t => t.TechnicianID == id)
+                .FirstOrDefault();
+            ClearChangeTracker();
+
+            if (tech == null)
+            {
+                return NotFound();
+            }
+
+            // Set the appropriate team properties and validate the team
+            model.Technician!.TechnicianID = tech.TechnicianID;
+            model.Technician.Name = tech.Name;
+
+            ValidateTeamEditViewModel(model);
+
+            // Show the edit form again if there were validation errors
+            if (!ModelState.IsValid)
+            {
+                model.Mode = "Edit";
+
+                return View("Edit", model);
+            }
+
+            // Update the team
+            _context.Update(model.Technician);
+            _context.SaveChanges();
+
+            // Redirect to the user's teams page
+            TempData["message"] = $"You just edited the team {model.Technician.Name}.";
+            return RedirectToAction("List", "Technician");
+        }
+
+        [HttpGet]
+        [Route("/tech/{id}/delete/")]
+        public ActionResult Delete(int id)
+        {
+            // Ensure the user is logged in
+            var model = new Technician();
+
+
+            // Verify the character exists and is owned by the logged in user
+            model = _context.Technicians
+                .Where(c => c.TechnicianID == id)
+                .FirstOrDefault();
+
+            if (model == null)
+            {
+                return NotFound();
+            }
+
+            return View("Delete", model);
+        }
+
+        [HttpPost]
+        [Route("/tech/{id}/delete/")]
+        public ActionResult Delete(int id, Technician model)
+        {
+
+            // Ensure the character exists and is owned by the logged in user
+            var Technician = _context.Technicians
+                .Where(c => c.TechnicianID == id)
+                .FirstOrDefault();
+
+            if (Technician == null)
+            {
+                return NotFound();
+            }
+
+
+
+            // Remove the character
+            _context.Remove(Technician);
+            _context.SaveChanges();
+
+            // Return to the user's character list page
+            TempData["message"] = $"You just deleted the character {Technician.Name}.";
+            return RedirectToAction("List", "Technician");
+        }
+
+    }
+}
