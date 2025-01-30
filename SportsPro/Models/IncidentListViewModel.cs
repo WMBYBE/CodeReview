674220f025dc962aaed13528ebb1d716957efd02
Grym3038 <156126041+Grym3038@users.noreply.github.com>
2025-01-30 17:30:38 -0600

    created the view models for 8-2

---
+++ SportsPro/Models/IncidentListViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace SportsPro.Models
+{
+    public class IncidentListViewModel
+    {
+        public List<Incident> Incidents { get; set; }
+        public string FilterResult { get; set; }
+    }
+}
