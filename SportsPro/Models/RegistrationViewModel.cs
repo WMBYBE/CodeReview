7a0f9bac888b06be6b71bd812ae86f440fa7cd59
WMBYBE <156222684+WMBYBE@users.noreply.github.com>
2025-04-06 21:08:32 -0500

    Will branch (#31)
    * created list titles incidents and a filter value for the view model
    
    * fixed filter name
    
    * Fixed some formatting and title info for the incidents
    
    * added semicolons after new sets
    
    * updated list action to support filter with a switch statement
    
    * used asp-route filters to add filter tabs to the incident list
    
    * updated value check to fix unassigned incidents
    
    * updated routing
    
    * 12-1
    
    ---------
    
    Co-authored-by: Grym3038 <156126041+Grym3038@users.noreply.github.com>

---
+++ SportsPro/Models/RegistrationViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace SportsPro.Models
+{
+    public class RegistrationViewModel
+    {
+        public Customer Customer { get; set; }
+        public List<Product> RegisteredProducts { get; set; }
+        public List<Product> AvailableProducts { get; set; }
+    }
+}
